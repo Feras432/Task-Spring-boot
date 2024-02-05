f15796b9ce15308ba44c063fcf1e5202ac9f39bd
Feras432 <firasshdad@gmail.com>
2024-02-05 09:45:48 +0300

    Third Commit

---
+++ demo/src/main/java/com/example/demo/bo/CreateContactRequest.java
@@ -0,0 +1,16 @@
+package com.example.demo.bo;
+
+public class CreateContactRequest {
+    private String name;
+    public String getName() {
+        return name;
+    }
+    private String email;
+    public String getEmail(){
+        return email;
+    }
+    private String phone;
+    public String getPhone(){
+        return phone;
+    }
+}
--- demo/src/main/java/com/example/demo/controller/CodedController.java
+++ demo/src/main/java/com/example/demo/controller/CodedController.java
@@ -1,11 +1,19 @@
 package com.example.demo.controller;
 
+import com.example.demo.bo.CreateContactRequest;
 import com.example.demo.bo.CreateFarewellRequest;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @RestController("/API/A-Z/")
 
 public class CodedController {
+
+    List<CreateContactRequest> allContact = new ArrayList<>();
     @GetMapping("/sayHi")
     public String sayHi(){
         return "Hello from spring boot";
@@ -19,5 +27,25 @@ public class CodedController {
     @PostMapping("/farewell")
     public String farewell(@RequestBody CreateFarewellRequest createFarewellRequest){
         return "Goodbye, " + createFarewellRequest.getName();
+    }
+    @PostMapping("/addContact")
+    public ResponseEntity<String> addContact(@RequestBody CreateContactRequest createContactRequest){
+        for(int i=0; i<allContact.size(); i++){
+          if (allContact.get(i).getEmail().equals(createContactRequest.getEmail())){
+              return ResponseEntity.badRequest().body("Contact already exists");
+          }
+
+        }
+        allContact.add(createContactRequest);
+        return ResponseEntity.ok("Contact added successfully!");
+    }
+    @GetMapping("/getContactDetails")
+    public ResponseEntity<?> getContactDetails(@RequestParam String name){
+        for (int i=0;i<allContact.size();i++){
+            if (allContact.get(i).getName().equals(name)){
+                return ResponseEntity.ok(allContact.get(i));
+            }
+        }
+        return ResponseEntity.badRequest().body("Contact not found");
     }
 }
